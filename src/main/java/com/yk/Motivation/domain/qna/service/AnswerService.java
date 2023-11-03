9950d433a9136105c4366c9cb66a6c72b254752a
g2jkj0274 <g2jkj0274@gmail.com>
2023-11-03 11:40:51 +0900

    Edit qna details, add comment

--- src/main/java/com/yk/Motivation/domain/qna/service/AnswerService.java
+++ src/main/java/com/yk/Motivation/domain/qna/service/AnswerService.java
@@ -1,5 +1,7 @@
 package com.yk.Motivation.domain.qna.service;
 
+import com.yk.Motivation.domain.qna.entity.Answer;
+import com.yk.Motivation.domain.qna.entity.Question;
 import com.yk.Motivation.domain.qna.repository.AnswerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,11 @@ import org.springframework.stereotype.Service;
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
 }
