ce3ec30084c24200ad69075b9af22b44426b44f7
g2jkj0274 <g2jkj0274@gmail.com>
2023-11-13 13:08:17 +0900

    backup

--- src/main/java/com/yk/Motivation/domain/qna/repository/QuestionRepository.java
+++ src/main/java/com/yk/Motivation/domain/qna/repository/QuestionRepository.java
@@ -1,14 +1,9 @@
 package com.yk.Motivation.domain.qna.repository;
 
 import com.yk.Motivation.domain.qna.entity.Question;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findByOrderByIdDesc(Pageable pageable);
-    List<Question> findByIdLessThanOrderByIdDesc(long lastId, Pageable pageable);
 }
