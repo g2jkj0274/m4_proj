3e3794ebcc73ded2a28fe1e30969a820bf55e758
g2jkj0274 <g2jkj0274@gmail.com>
2023-11-13 11:32:58 +0900

    backup

--- src/main/java/com/yk/Motivation/domain/qna/service/QuestionService.java
+++ src/main/java/com/yk/Motivation/domain/qna/service/QuestionService.java
@@ -6,6 +6,7 @@ import com.yk.Motivation.domain.qna.entity.Question;
 import com.yk.Motivation.domain.qna.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,18 @@ import java.util.Optional;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+
+    public List<Question> findLatestAfterId(int count, long lastId) {
+        PageRequest pageRequest = PageRequest.of(0, count);
+
+        if (lastId == 0) {
+            // 만약 lastId가 0이면 최신 질문부터 시작
+            return questionRepository.findByOrderByIdDesc(pageRequest);
+        }
+
+        // lastId 이후의 질문을 불러옴
+        return questionRepository.findByIdLessThanOrderByIdDesc(lastId, pageRequest);
+    }
 
     public Optional<Question> findById(Long id) {
         return questionRepository.findById(id);
